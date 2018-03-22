a314af38a11f9c4797705171ee771d64e0a90539
Shashank Srivatsa Kuchibhotla <sk.kuchibhotla@gmail.com>
2018-03-21 22:49:09 -0400

    Module 1: Initial Build, Dependency Injection

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessageOfTheDay()
+        {
+            //throw new System.NotImplementedException();
+            //return "Greetings!";
+            return _configuration["Greeting"];
+        }
+    }
+}
